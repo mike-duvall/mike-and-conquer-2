38903e046308bd476c9f8658c90cde4967a53272
Mike DuVall <mike.a.duvall@gmail.com>
2016-07-05 07:04:34 -0500

    Initial add of generated project

---
+++ desktop/src/com/mikeduvall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mikeduvall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mikeduvall.MikeAndConquerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MikeAndConquerGame(), config);
+	}
+}
