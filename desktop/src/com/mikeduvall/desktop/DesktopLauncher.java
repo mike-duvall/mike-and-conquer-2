622ffacb8047d1e54f234d438f1a349ca3d9560f
Mike DuVall <mike.a.duvall@gmail.com>
2016-07-06 07:19:23 -0500

    Added sample code

--- desktop/src/com/mikeduvall/desktop/DesktopLauncher.java
+++ desktop/src/com/mikeduvall/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.mikeduvall.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cookbook.samples.SpriteBatchSample;
 import com.mikeduvall.MikeAndConquerGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MikeAndConquerGame(), config);
+//		new LwjglApplication(new MikeAndConquerGame(), config);
+		new LwjglApplication(new SpriteBatchSample(),  config);
 	}
 }
